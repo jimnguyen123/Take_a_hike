3061663e17735850a7083cdd4d5380fc9bc1627e
Natasha Springer <tasha@Natashas-MacBook-Pro.local>
2014-12-11 21:40:11 -0800

    added trail pages

--- routes/index.js
+++ routes/index.js
@@ -56,4 +56,105 @@ router.get('/tips', function(req, res) {
 router.get('/blah', function(req, res) {
   res.render('blah', { title: 'Blah Page' });
 });
+
+
+
+
+/* GET template page. */
+router.get('/hike1', function(req, res) {
+  res.render('hike1', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike2', function(req, res) {
+  res.render('hike2', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike3', function(req, res) {
+  res.render('hike3', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike4', function(req, res) {
+  res.render('hike4', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike5', function(req, res) {
+  res.render('hike5', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike6', function(req, res) {
+  res.render('hike6', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike7', function(req, res) {
+  res.render('hike7', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike8', function(req, res) {
+  res.render('hike8', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike9', function(req, res) {
+  res.render('hike9', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike10', function(req, res) {
+  res.render('hike10', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike11', function(req, res) {
+  res.render('hike11', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike12', function(req, res) {
+  res.render('hike12', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike13', function(req, res) {
+  res.render('hike13', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike14', function(req, res) {
+  res.render('hike14', { title: 'Blah Page' });
+});
+/* GET template page. */
+router.get('/hike15', function(req, res) {
+  res.render('hike15', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike16', function(req, res) {
+  res.render('hike16', { title: 'Blah Page' });
+});
+
+/* GET template page. */
+router.get('/hike17', function(req, res) {
+  res.render('hike17', { title: 'Blah Page' });
+});
+
+
+/* GET template page. */
+router.get('/hike18', function(req, res) {
+  res.render('hike18', { title: 'Blah Page' });
+});
+
+
+/* GET template page. */
+router.get('/hike19', function(req, res) {
+  res.render('hike19', { title: 'Blah Page' });
+});
+
+
+/* GET template page. */
+router.get('/hike20', function(req, res) {
+  res.render('hike20', { title: 'Blah Page' });
+});
+
 module.exports = router;
